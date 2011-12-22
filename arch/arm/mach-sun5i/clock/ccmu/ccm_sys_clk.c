b87a36850da4fd6c82aee64cd142d03deae01c15
kevin <kevin@allwinnertech.com>
2011-12-22 16:18:08 +0800

    update ccu for sun5i, fix bug for pll2 config

--- arch/arm/mach-sun5i/clock/ccmu/ccm_sys_clk.c
+++ arch/arm/mach-sun5i/clock/ccmu/ccm_sys_clk.c
@@ -265,7 +265,7 @@ static __s64 sys_clk_get_rate(__aw_ccu_sys_clk_e id)
             {
                 /* set audio pll to default value 24576000 */
                 tmpReg &= ~((0x1f<<0)|(0x7f<<8)|(0x0f<<26));
-                tmpReg |= (21<<0)|(86<<8)|(4<<26);
+                tmpReg |= (21<<0)|(86<<8)|(3<<26);
                 *(volatile __u32 *)&aw_ccu_reg->Pll2Ctl = tmpReg;
                 return 24576000;
             }
