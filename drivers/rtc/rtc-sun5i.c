3393e36afe05a0406c0fffab1fc0132e0bf621df
huangxin <huangxin@MServer.allwinnertech.com>
2011-12-26 17:27:21 +0800

    sun5i:modify the rtc drv for i2c bus

--- drivers/rtc/rtc-sun5i.c
+++ drivers/rtc/rtc-sun5i.c
@@ -197,7 +197,7 @@ static int pcf8563_get_datetime(struct i2c_client *client, struct rtc_time *tm)
 		buf[0], buf[1], buf[2], buf[3],
 		buf[4], buf[5], buf[6], buf[7],
 		buf[8]);
-
+ 
 
 	tm->tm_sec = bcd2bin(buf[PCF8563_REG_SC] & 0x7F);
 	tm->tm_min = bcd2bin(buf[PCF8563_REG_MN] & 0x7F);
