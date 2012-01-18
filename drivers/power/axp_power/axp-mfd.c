d33c1dd085ce3bdc9e691c4280d6a1e2120d70b2
huangxin <huangxin@MServer.allwinnertech.com>
2012-01-18 17:55:47 +0800

    sun5i:rtc:suppose the alarm

--- drivers/power/axp_power/axp-mfd.c
+++ drivers/power/axp_power/axp-mfd.c
@@ -295,7 +295,7 @@ static int __devinit axp_mfd_probe(struct i2c_client *client,
 		goto out_free_chip;
 
 	ret = request_irq(client->irq, axp_mfd_irq_handler,
-		IRQF_DISABLED, "axp_mfd", chip);
+		IRQF_SHARED|IRQF_DISABLED, "axp_mfd", chip);
   	if (ret) {
   		dev_err(&client->dev, "failed to request irq %d\n",
   				client->irq);
