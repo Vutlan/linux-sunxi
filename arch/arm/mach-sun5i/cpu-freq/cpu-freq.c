cb3ae24785a5900571e883fa703f794c88ba7d10
kevin <kevin@allwinnertech.com>
2012-01-20 09:55:57 +0800

    update v-f table for cpu-freq, set cpu vdd to 0.9v for standby

--- arch/arm/mach-sun5i/cpu-freq/cpu-freq.c
+++ arch/arm/mach-sun5i/cpu-freq/cpu-freq.c
@@ -43,14 +43,15 @@ struct cpufreq_dvfs {
     unsigned int    volt;   /* voltage for the frequency    */
 };
 static struct cpufreq_dvfs dvfs_table[] = {
-    {.freq = 1056000000, .volt = 1500}, /* core vdd is 1.50v if cpu frequency is (1008Mhz, xxxxMhz] */
-    {.freq = 1008000000, .volt = 1400}, /* core vdd is 1.40v if cpu frequency is (960Mhz, 1008Mhz]  */
-    {.freq = 960000000,  .volt = 1400}, /* core vdd is 1.40v if cpu frequency is (912Mhz, 960Mhz]   */
-    {.freq = 912000000,  .volt = 1350}, /* core vdd is 1.35v if cpu frequency is (864Mhz, 912Mhz]   */
-    {.freq = 864000000,  .volt = 1300}, /* core vdd is 1.30v if cpu frequency is (624Mhz, 864Mhz]   */
-    {.freq = 624000000,  .volt = 1250}, /* core vdd is 1.25v if cpu frequency is (432Mhz, 624Mhz]   */
-    {.freq = 432000000,  .volt = 1250}, /* core vdd is 1.25v if cpu frequency is (0, 432Mhz]        */
-    {.freq = 0,          .volt = 1000}, /* end of cpu dvfs table                                    */
+    {.freq = 1104000000, .volt = 1500}, /* core vdd is 1.50v if cpu frequency is (1104Mhz, xxxxMhz] */
+    {.freq = 1008000000, .volt = 1400}, /* core vdd is 1.40v if cpu frequency is (1008Mhz, 1104Mhz] */
+    {.freq = 912000000,  .volt = 1350}, /* core vdd is 1.35v if cpu frequency is (912Mhz, 1008Mhz]  */
+    {.freq = 864000000,  .volt = 1300}, /* core vdd is 1.30v if cpu frequency is (864Mhz, 912Mhz]   */
+    {.freq = 624000000,  .volt = 1200}, /* core vdd is 1.20v if cpu frequency is (624Mhz, 864Mhz]   */
+    {.freq = 576000000,  .volt = 1100}, /* core vdd is 1.10v if cpu frequency is (576Mhz, 624Mhz]   */
+    {.freq = 432000000,  .volt = 1000}, /* core vdd is 1.00v if cpu frequency is (240Mhz, 576Mhz]   */
+    {.freq = 240000000,  .volt = 900 }, /* core vdd is 0.90v if cpu frequency is (0, 240Mhz]        */
+    {.freq = 0,          .volt = 800 }, /* end of cpu dvfs table                                    */
 };
 static struct regulator *corevdd;
 static unsigned int last_vdd    = 1400;     /* backup last target voltage, default is 1.4v  */
