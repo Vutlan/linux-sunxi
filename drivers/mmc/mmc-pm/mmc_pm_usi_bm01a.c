5dce603de0b62fe22242ccfe4205c737dd43ce7d
yemao <leafy.myeh@allwinnertech.com>
2012-02-22 20:22:10 +0800

    wifi:usi-bm01a:delay some time after power up for more stability

--- drivers/mmc/mmc-pm/mmc_pm_usi_bm01a.c
+++ drivers/mmc/mmc-pm/mmc_pm_usi_bm01a.c
@@ -144,6 +144,7 @@ power_change:
         usi_msg("Failed to power off USI-BM01A module!\n");
         return -1;
     }
+    mdelay(5);
     ret = gpio_write_one_pin_value(ops->pio_hdle, level, "usi_bm01a_wlbt_regon");
     if (ret) {
         usi_msg("Failed to regon off for  USI-BM01A module!\n");
