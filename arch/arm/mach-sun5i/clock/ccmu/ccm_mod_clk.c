7aa9ac96518df0a3b6ca434006a489b4124b3a4a
kevin <kevin@allwinnertech.com>
2012-03-14 19:00:04 +0800

    update ccmu for sun5i, fix usb clock bug

--- arch/arm/mach-sun5i/clock/ccmu/ccm_mod_clk.c
+++ arch/arm/mach-sun5i/clock/ccmu/ccm_mod_clk.c
@@ -1610,17 +1610,10 @@ static __s32 mod_clk_set_reset(__aw_ccu_mod_clk_e id, __aw_ccu_clk_reset_e reset
         case AW_MOD_CLK_USBPHY:
         case AW_MOD_CLK_USBOHCI0:
             return (reset == AW_CCU_CLK_NRESET)? 0 : -1;
-
         case AW_MOD_CLK_USBPHY0:
-        {
-            aw_ccu_reg->UsbClk.UsbPhy0Rst = (reset == AW_CCU_CLK_RESET)? 0 : 1;
-            return 0;
-        }
         case AW_MOD_CLK_USBPHY1:
-        {
-            aw_ccu_reg->UsbClk.UsbPhy1Rst = (reset == AW_CCU_CLK_RESET)? 0 : 1;
             return 0;
-        }
+
         case AW_MOD_CLK_GPS:
         {
             aw_ccu_reg->GpsClk.Reset = (reset == AW_CCU_CLK_RESET)? 0 : 1;
