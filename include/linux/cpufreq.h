cee3ae292c76212aad94d11b7ce5ed80551f4dc7
kevin <kevin@LServer2.(none)>
2012-07-04 15:38:47 +0800

    1. add interface for ar100; 2. add cpu frequency sclaing driver for dvfs; 3. add fantasys dvfs policy

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -363,6 +363,10 @@ extern struct cpufreq_governor cpufreq_gov_ondemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_FANTASYS)
+extern struct cpufreq_governor cpufreq_gov_fantasys;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_fantasys)
+
 #endif
 
 
