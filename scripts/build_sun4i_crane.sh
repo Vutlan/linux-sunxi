e28175a37f428f503c7002441205d5cc0e9143a7
wujinliang <wujinliang@allwinnertech.com>
2012-10-22 17:14:41 +0800

    delete old bcm40183 driver

--- scripts/build_sun4i_crane.sh
+++ scripts/build_sun4i_crane.sh
@@ -159,11 +159,6 @@ build_modules()
 			CROSS_COMPILE=${CROSS_COMPILE} ARCH=arm LINUXVER=${KERNEL_VERSION} \
 			LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LINUXDIR=${LICHEE_KDIR} CONFIG_CHIP_ID=${CONFIG_CHIP_ID} \
 			INSTALL_DIR=${LICHEE_MOD_DIR} dhd-cdc-sdmmc-gpl
-	#build bcm40183 sdio wifi module
-	make -C modules/wifi/bcm40183/5.90.125.95.3/open-src/src/dhd/linux \
-			CROSS_COMPILE=${CROSS_COMPILE} ARCH=arm LINUXVER=${KERNEL_VERSION} \
-			LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LINUXDIR=${LICHEE_KDIR} CONFIG_CHIP_ID=${CONFIG_CHIP_ID} \
-			INSTALL_DIR=${LICHEE_MOD_DIR} OEM_ANDROID=1 dhd-cdc-sdmmc-gpl
 
 	#copy gps driver to module
 	cp modules/gps/gps.ko_ $LICHEE_MOD_DIR/gps.ko
@@ -219,11 +214,6 @@ clean_modules()
 			CROSS_COMPILE=${CROSS_COMPILE} ARCH=arm LINUXVER=${KERNEL_VERSION} \
 			LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LINUXDIR=${LICHEE_KDIR} CONFIG_CHIP_ID=${CONFIG_CHIP_ID} \
 			INSTALL_DIR=${LICHEE_MOD_DIR} clean
-	#build bcm40183 sdio wifi module
-	make -C modules/wifi/bcm40183/5.90.125.95.3/open-src/src/dhd/linux \
-			CROSS_COMPILE=${CROSS_COMPILE} ARCH=arm LINUXVER=${KERNEL_VERSION} \
-			LICHEE_MOD_DIR=${LICHEE_MOD_DIR} LINUXDIR=${LICHEE_KDIR} CONFIG_CHIP_ID=${CONFIG_CHIP_ID} \
-			INSTALL_DIR=${LICHEE_MOD_DIR} OEM_ANDROID=1 clean
 }
 
 #####################################################################
