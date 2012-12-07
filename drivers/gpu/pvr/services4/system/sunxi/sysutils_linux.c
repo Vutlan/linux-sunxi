48c4ff067693ef68501ba54e739a94148107a1d8
yanggq <yanggq@LServer2.(none)>
2012-12-07 15:51:29 +0800

    standby[gpu]: enable gpu clk when resume.

--- drivers/gpu/pvr/services4/system/sunxi/sysutils_linux.c
+++ drivers/gpu/pvr/services4/system/sunxi/sysutils_linux.c
@@ -363,13 +363,6 @@ PVRSRV_ERROR EnableSystemClocks(SYS_DATA *psSysData)
 			printk("set gpu_coreclk parent to gpu_corepll failed!\n");
 		}
 
-		/*open pll, in EnableSystemClocks temporarily*/
-		if(clk_enable(h_gpu_hydpll)){
-			printk("try to enable gpu_hydpll output failed!\n");
-		}
-		if(clk_enable(h_gpu_corepll)){
-			printk("try to enable gpu_corepll output failed!\n");
-		}
 			
 		mutex_init(&psSysSpecData->sPowerLock);
 
@@ -378,6 +371,14 @@ PVRSRV_ERROR EnableSystemClocks(SYS_DATA *psSysData)
 		psSysSpecData->bSysClocksOneTimeInit = IMG_TRUE;
 	}
 
+	/*open pll, in EnableSystemClocks temporarily*/
+	if(clk_enable(h_gpu_hydpll)){
+		printk("try to enable gpu_hydpll output failed!\n");
+	}
+	if(clk_enable(h_gpu_corepll)){
+		printk("try to enable gpu_corepll output failed!\n");
+	}
+		
 	return AcquireGPTimer(psSysSpecData);
 }
 
