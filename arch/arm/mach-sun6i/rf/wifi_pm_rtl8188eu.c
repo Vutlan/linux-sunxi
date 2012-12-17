618d16dcfaf8c0473f236d080986d0229060f1e9
wujinliang <wujinliang@allwinnertech.com>
2012-12-17 10:02:13 +0800

    [wifi-bt]: 1.update usb wifi power manager, 2.add bluetooth support

--- arch/arm/mach-sun6i/rf/wifi_pm_rtl8188eu.c
+++ arch/arm/mach-sun6i/rf/wifi_pm_rtl8188eu.c
@@ -10,7 +10,6 @@
 #include "wifi_pm.h"
 
 #define rtl8188eu_msg(...)    do {printk("[rtl8188eu]: "__VA_ARGS__);} while(0)
-//#define PLATFORM_33_EVB
 
 static int rtl8188eu_powerup = 0;
 static int rtl8188eu_power_pin = 0;
@@ -42,48 +41,14 @@ static int rtl8188eu_module_power(int onoff)
 	return 0;
 }
 
-// power control by gpio
-static int rtl8188eu_gpio_ctrl(char* name, int level)
-{
-	int i = 0, ret = 0, gpio = 0;
-	unsigned long flags = 0;
-
-	gpio = rtl8188eu_power_pin;
-	
-	if (1==level)
-		flags = GPIOF_OUT_INIT_HIGH;
-	else
-		flags = GPIOF_OUT_INIT_LOW;
-
-	ret = gpio_request_one(gpio, flags, NULL);
-	if (ret) {
-		rtl8188eu_msg("failed to set gpio %s to %d !\n", name, level);
-		return -1;
-	} else {
-		gpio_free(gpio);
-		rtl8188eu_msg("succeed to set gpio %s to %d !\n", name, level);
-	}
-
-    rtl8188eu_powerup = level;
-	return 0;
-}
-
 void rtl8188eu_power(int mode, int *updown)
 {
     if (mode) {
         if (*updown) {
-#ifdef PLATFORM_33_EVB
-			rtl8188eu_gpio_ctrl("rtl8188eu_power_pin", 1);
-#else
 			rtl8188eu_module_power(1);
-#endif
 			udelay(50);
         } else {
-#ifdef PLATFORM_33_EVB
-			rtl8188eu_gpio_ctrl("rtl8188eu_power_pin", 0);
-#else
 			rtl8188eu_module_power(0);
-#endif
         }
         rtl8188eu_msg("sdio wifi power state: %s\n", *updown ? "on" : "off");
     } else {
@@ -100,20 +65,12 @@ static void rtl8188eu_standby(int instadby)
 {
 	if (instadby) {
 		if (rtl8188eu_powerup) {
-#ifdef PLATFORM_33_EVB
-			rtl8188eu_gpio_ctrl("rtl8188eu_power_pin", 0);
-#else
 			rtl8188eu_module_power(0);
-#endif
 			rtk8188eu_suspend = 1;
 		}
 	} else {
 		if (rtk8188eu_suspend) {
-#ifdef PLATFORM_33_EVB
-			rtl8188eu_gpio_ctrl("rtl8188eu_power_pin", 1);
-#else
 			rtl8188eu_module_power(1);
-#endif
 			rtk8188eu_suspend = 0;
 		}
 	}
@@ -127,12 +84,6 @@ void rtl8188eu_gpio_init(void)
 	struct wifi_pm_ops *ops = &wifi_select_pm_ops;
 
 	rtl8188eu_msg("exec rtl8188eu_wifi_gpio_init\n");
-
-	type = script_get_item(wifi_para, "rtl8188eu_power", &val);
-	if (SCIRPT_ITEM_VALUE_TYPE_PIO!=type) 
-		rtl8188eu_msg("get rtl8188eu rtl8188eu_power gpio failed\n");
-	else
-		rtl8188eu_power_pin = val.gpio.gpio;
 
 	rtl8188eu_powerup = 0;
 	rtk8188eu_suspend = 0;
--- arch/arm/mach-sun6i/rf/wifi_pm_rtl8192cu.c
+++ arch/arm/mach-sun6i/rf/wifi_pm_rtl8192cu.c
@@ -10,7 +10,6 @@
 #include "wifi_pm.h"
 
 #define rtl8192cu_msg(...)    do {printk("[rtl8192cu]: "__VA_ARGS__);} while(0)
-#define PLATFORM_33_EVB
 
 static int rtl8192cu_powerup = 0;
 static int rtl8192cu_power_pin = 0;
@@ -42,48 +41,14 @@ static int rtl8192cu_module_power(int onoff)
 	return 0;
 }
 
-// power control by gpio
-static int rtl8192cu_gpio_ctrl(char* name, int level)
-{
-	int i = 0, ret = 0, gpio = 0;
-	unsigned long flags = 0;
-
-	gpio = rtl8192cu_power_pin;
-	
-	if (1==level)
-		flags = GPIOF_OUT_INIT_HIGH;
-	else
-		flags = GPIOF_OUT_INIT_LOW;
-
-	ret = gpio_request_one(gpio, flags, NULL);
-	if (ret) {
-		rtl8192cu_msg("failed to set gpio %s to %d !\n", name, level);
-		return -1;
-	} else {
-		gpio_free(gpio);
-		rtl8192cu_msg("succeed to set gpio %s to %d !\n", name, level);
-	}
-
-    rtl8192cu_powerup = level;
-	return 0;
-}
-
 void rtl8192cu_power(int mode, int *updown)
 {
     if (mode) {
         if (*updown) {
-#ifdef PLATFORM_33_EVB
-			rtl8192cu_gpio_ctrl("rtl8192cu_power_pin", 1);
-#else
 			rtl8192cu_module_power(1);
-#endif
 			udelay(50);
         } else {
-#ifdef PLATFORM_33_EVB
-			rtl8192cu_gpio_ctrl("rtl8192cu_power_pin", 0);
-#else
 			rtl8192cu_module_power(0);
-#endif
         }
         rtl8192cu_msg("sdio wifi power state: %s\n", *updown ? "on" : "off");
     } else {
@@ -100,20 +65,12 @@ static void rtl8192cu_standby(int instadby)
 {
 	if (instadby) {
 		if (rtl8192cu_powerup) {
-#ifdef PLATFORM_33_EVB
-			rtl8192cu_gpio_ctrl("rtl8192cu_power_pin", 0);
-#else
 			rtl8192cu_module_power(0);
-#endif
 			rtk8192cu_suspend = 1;
 		}
 	} else {
 		if (rtk8192cu_suspend) {
-#ifdef PLATFORM_33_EVB
-			rtl8192cu_gpio_ctrl("rtl8192cu_power_pin", 1);
-#else
 			rtl8192cu_module_power(1);
-#endif
 			rtk8192cu_suspend = 0;
 		}
 	}
@@ -127,12 +84,6 @@ void rtl8192cu_gpio_init(void)
 	struct wifi_pm_ops *ops = &wifi_select_pm_ops;
 
 	rtl8192cu_msg("exec rtl8192cu_wifi_gpio_init\n");
-
-	type = script_get_item(wifi_para, "rtl8192cu_power", &val);
-	if (SCIRPT_ITEM_VALUE_TYPE_PIO!=type) 
-		rtl8192cu_msg("get rtl8192cu rtl8192cu_power gpio failed\n");
-	else
-		rtl8192cu_power_pin = val.gpio.gpio;
 
 	rtl8192cu_powerup = 0;
 	rtk8192cu_suspend = 0;
