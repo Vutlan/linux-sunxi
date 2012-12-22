5b63b5ea35ff0c07309b050977a839ef5db45e07
wangjx <wangjx@allwinnertech.com>
2012-12-22 12:32:02 +0800

    fix:usb phy disconnect value

--- drivers/usb/host/sw_hci_sun6i.c
+++ drivers/usb/host/sw_hci_sun6i.c
@@ -472,7 +472,7 @@ static void UsbPhyInit(__u32 usbc_no)
 //	DMSG_INFO("csr2-1: usbc%d: 0x%x\n", usbc_no, (u32)USBC_Phy_Read(usbc_no, 0x20, 5));
 
     /* µ÷½Ú disconnect ÓòÖµ */
-	USBC_Phy_Write(usbc_no, 0x2a, 2, 2);    /*by wangjx*/
+	USBC_Phy_Write(usbc_no, 0x2a, 3, 2);    /*by wangjx*/
 
 //	DMSG_INFO("csr2: usbc%d: 0x%x\n", usbc_no, (u32)USBC_Phy_Read(usbc_no, 0x2a, 2));
 //	DMSG_INFO("csr3: usbc%d: 0x%x\n", usbc_no, (u32)USBC_Readl(USBC_Phy_GetCsr(usbc_no)));
